72086d30061112fd22afa60d72663c3209181e26
artem <artem.derkach@itrexgroup.com>
2019-05-30 15:56:26 +0300

    add yaml file handler

--- 2_urlshort/main/main.go
+++ 2_urlshort/main/main.go
@@ -1,38 +1,56 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"io/ioutil"
 	"net/http"
+	"os"
 
 	urlshort "github.com/gophercises/2_urlshort"
 )
+
+var defaultMap = map[string]string{
+	"/urlshort-godoc": "https://godoc.org/github.com/gophercises/urlshort",
+	"/yaml-godoc":     "https://godoc.org/gopkg.in/yaml.v2",
+}
 
 func main() {
 	mux := defaultMux()
 
 	// Build the MapHandler using the mux as the fallback
-	pathsToUrls := map[string]string{
-		"/urlshort-godoc": "https://godoc.org/github.com/gophercises/urlshort",
-		"/yaml-godoc":     "https://godoc.org/gopkg.in/yaml.v2",
-	}
-	mapHandler := urlshort.MapHandler(pathsToUrls, mux)
+	mapHandler := urlshort.MapHandler(defaultMap, mux)
 
 	// Build the YAMLHandler using the mapHandler as the fallback
-	yaml := `
-- path: /urlshort
-  url: https://github.com/gophercises/urlshort
-- path: /urlshort-final
-  url: https://github.com/gophercises/urlshort/tree/solution
-`
-
-	// - path: /urlshort-final
-	//   url: https://github.com/gophercises/urlshort/tree/solution
-	yamlHandler, err := urlshort.YAMLHandler([]byte(yaml), mapHandler)
+	yamlHandler, err := urlshort.YAMLHandler(parseYaml(), mapHandler)
 	if err != nil {
 		panic(err)
 	}
 	fmt.Println("Starting the server on :8080")
 	http.ListenAndServe(":8080", yamlHandler)
+}
+
+func parseYaml() []byte {
+	var defaultYamlMap = []byte(`
+- path: /urlshort
+  url: https://github.com/gophercises/urlshort
+- path: /urlshort-final
+  url: https://github.com/gophercises/urlshort/tree/solution
+`)
+	yamlFilePath := flag.String("yamlFile", "", "yaml file to read urls and paths from")
+	flag.Parse()
+	if *yamlFilePath == "" {
+		return defaultYamlMap
+	}
+	yamlFile, err := os.Open(*yamlFilePath)
+	if err != nil {
+		panic(err)
+	}
+	yamlFileContent, err := ioutil.ReadAll(yamlFile)
+	if err != nil {
+		panic(err)
+	}
+	return yamlFileContent
 }
 
 func defaultMux() *http.ServeMux {
